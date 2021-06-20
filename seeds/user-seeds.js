8e2adda0abcd3b4c70967910ff31a0ccbab01122
bdcii <darrell.chess@gmail.com>
2021-06-20 16:35:12 -0400

    created seed files, added user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    username: "michaelangelo",
+    email: "mikeytmnt@gmail.com",
+    password: "p1zzat1m3"
+  },
+  {
+    username: "donatello",
+    email: "donnytmnt@gmail.com",
+    password: "turtlepower"
+  },
+  {
+    username: "raphael",
+    email: "ralphtmnt@gmail.com",
+    password: "cowabunga1234"
+  },
+  {
+    username: "leonardo",
+    email: "leotmnt@gmail.com",
+    password: "password1234"
+  }
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
