b947f581a81bc8b259560e4495508f9f4966dd79
bdcii <darrell.chess@gmail.com>
2021-06-20 18:22:23 -0400

    added controller index info, create route controller files

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,16 @@
-//Index for models and their associations
+//Dependencies
+const router = require('express').Router();
 
-const User = require('./User');
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
